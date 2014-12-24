c1c3441c20320e8717118d558ef394e268743495
Chef21 <vsayan@hotmail.com>
2014-12-24 14:03:02 +0100

    Fullscreen in GameOverActivity

--- FastTouch/src/com/example/fasttouch/GameOverActivity.java
+++ FastTouch/src/com/example/fasttouch/GameOverActivity.java
@@ -8,6 +8,8 @@ import android.content.SharedPreferences.Editor;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class GameOverActivity extends Activity {
@@ -19,6 +21,8 @@ public class GameOverActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE); 
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_gameover);
 		
 		final MediaPlayer mp_gameover = MediaPlayer.create(getApplicationContext(),R.raw.gameover);
